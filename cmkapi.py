c70b56036023bce4ac3c646fd741c3dcabd2fa23
1337pwny <do5frx@darc.de>
2023-01-29 00:32:49 +0100

    Adds function for getHost and createHost

--- cmkapi.py
+++ cmkapi.py
@@ -1,20 +1,28 @@
 import requests
 import pprint
 class Service:
-    def __init__(self):
-        #bla
+    def __init__(self, name):
+        self.name = name
 # Types:
-# agent: Only Agent
-# agentSNMP: Agent + SNMP
+# cmk-agent: cmk Agent
+# no-agent: No CMK Agent
+# agentSNMP: snmp-v2
 # none: no type
+
+
 class Host:
-    def __init__(self, hostname, ip, labels=[], folder="/", type="agent"):
+    def __init__(self, hostname, ip, labels=[], folder="/", agentType="no-agent", snmpType=None):
         self.hostname = hostname
         self.ip = ip
         self.labels = labels
         self.folder = folder
-        self.type = type
-        #blab
+        self.agentType = agentType
+        self.snmpType = snmpType
+
+    def __str__(self):
+        return self.hostname+" "+self.ip+" "+self.folder+" "+self.agentType+" "+str(self.snmpType)+" "+str(self.labels)
+
+
 class CmkApi:
     def __init__(self, hostname, site, user, secret, protocoll="https", port="443"):
         self.hostname = hostname
@@ -27,28 +35,63 @@ class CmkApi:
         self.session.headers['Accept'] = 'application/json'
         self.protocoll = protocoll
         self.url = self.protocoll+"://"+self.hostname+":"+self.port+"/"+self.site+"/check_mk/api/1.0"
-    def getHostObjectFromResponse(self, response):
-        # ToDo: Add parsing from response
-        return Host()
+
+    def __getHostObjectFromResponse(self, response):
+        extensions = response.json()["extensions"]
+        hostname = response.json()["id"]
+        folder = extensions["folder"]
+        attrs = extensions["attributes"]
+        ipaddress = attrs["ipaddress"]
+        if "labels" in attrs:
+            labels = attrs["labels"]
+        else:
+            labels = None
+        if "tag_snmp_ds" in attrs:
+            snmpType = attrs["tag_snmp_ds"]
+        else:
+            snmpType = None
+        type = attrs["tag_agent"]
+        if folder == " ":
+            folder = "/"
+        return Host(hostname=hostname,ip=ipaddress,labels=labels,folder=folder,agentType=type, snmpType=snmpType)
+
     def getHost(self,hostname):
         resp = self.session.get(
             f"{self.url}/objects/host_config/"+hostname,
             params={  # goes into query string
                 "effective_attributes": False,
-                # Show all effective attributes, which affect this host, not just the attributes which were set on this host specifically.
             },
         )
         if resp.status_code == 200:
-            return self.getHostObjectFromResponse(resp)
+            return self.__getHostObjectFromResponse(resp)
         elif resp.status_code == 204:
-            print("Done")
             return None
         else:
-            raise RuntimeError(pprint.pformat(resp.json()))
             return None
     def getServiceForHost(hostname, service):
-        # bla
+        return None
     def addFolder(foldername, subpath="/"):
-        # bla
-    def addHost(host):
-        # bla
+        return None
+
+    def addHost(self, host):
+        url = f"{self.url}/domain-types/host_config/collections/all"
+        headers = {"Content-Type": 'application/json',}
+        json = {
+                   'folder': host.folder,
+                   'host_name': host.hostname,
+                   'attributes': {
+                       'ipaddress': host.ip,
+                       'tag_agent': host.agentType
+                   }
+               }
+        if host.labels is not None:
+            json["attributes"]["labels"] = host.labels
+        if host.snmpType is not None:
+            json["attributes"]["tag_snmp_ds"] = host.snmpType
+        resp = self.session.post(url=url,headers=headers,json=json)
+        if resp.status_code == 200:
+            return host
+        elif resp.status_code == 204:
+            return None
+        else:
+            return None
